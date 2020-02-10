3bd369266019e7169cf4fec9fa3507c956c44d20
Nikita Gazarov <nikita@raquo.com>
2020-02-09 23:13:43 -0800

    API: DynamicSubscription constructor helpers

--- src/main/scala/com/raquo/airstream/ownership/DynamicSubscription.scala
+++ src/main/scala/com/raquo/airstream/ownership/DynamicSubscription.scala
@@ -1,5 +1,11 @@
 package com.raquo.airstream.ownership
 
+import com.raquo.airstream.core.{Observable, Observer}
+import com.raquo.airstream.eventbus.WriteBus
+import com.raquo.airstream.eventstream.EventStream
+
+// @TODO[API] I could make the constructor public but it's less confusing if you use the companion object methods
+
 /** Represents a subscription that can be turned on and off repeatedly.
   * For example, in Laminar the elements can be mounted and unmounted repeatedly,
   * and so their subscriptions are activated and deactivated respectively when
@@ -12,31 +18,76 @@ package com.raquo.airstream.ownership
   *
   * @param activate Note: Must not throw!
   */
-class DynamicSubscription (
+class DynamicSubscription private (
   dynamicOwner: DynamicOwner,
-  activate: Owner => Subscription
+  activate: Owner => Option[Subscription]
 ) {
 
+  // @Note this can be None even if this dynamic subscription is active (if activate() returned None)
   private[this] var maybeCurrentSubscription: Option[Subscription] = None
 
   dynamicOwner.addSubscription(this)
 
-  @inline def isActive: Boolean = maybeCurrentSubscription.isDefined
+  @inline def isOwnerActive: Boolean = dynamicOwner.isActive
 
   /** Permanently kill this subscription, deactivating if it's currently active, and removing it from the dynamic owner */
   def kill(): Unit = dynamicOwner.removeSubscription(this)
 
   private[ownership] def onActivate(owner: Owner): Unit = {
-    maybeCurrentSubscription = Some(activate(owner))
+    maybeCurrentSubscription = activate(owner)
   }
 
   private[ownership] def onDeactivate(): Unit = {
-    maybeCurrentSubscription.fold {
-      throw new Exception("Can not deactivate DynamicSubscription: it is not active")
-    } { currentSubscription =>
+    maybeCurrentSubscription.foreach { currentSubscription =>
       currentSubscription.kill()
       maybeCurrentSubscription = None
     }
   }
 }
 
+object DynamicSubscription {
+
+  /** Use this when your activate() code requires cleanup on deactivation.
+    * Specify that cleanup code inside the resulting Subscription.
+    *
+    * @param activate Note: Must not throw!
+    */
+  def apply(dynamicOwner: DynamicOwner, activate: Owner => Subscription): DynamicSubscription = {
+    new DynamicSubscription(dynamicOwner, (owner: Owner) => Some(activate(owner)))
+  }
+
+  /** Use this when your activate() code does not require a cleanup on deactivation.
+    *
+    * @param activate Note: Must not throw!
+    */
+  def subscribeCallback(dynamicOwner: DynamicOwner, activate: Owner => Unit): DynamicSubscription = {
+    new DynamicSubscription(dynamicOwner, (owner: Owner) => {
+      activate(owner)
+      None
+    })
+  }
+
+  def subscribeObserver[A](
+    dynamicOwner: DynamicOwner,
+    observable: Observable[A],
+    observer: Observer[A]
+  ): DynamicSubscription = {
+    DynamicSubscription(dynamicOwner, owner => observable.addObserver(observer)(owner))
+  }
+
+  def subscribeFn[A](
+    dynamicOwner: DynamicOwner,
+    observable: Observable[A],
+    onNext: A => Unit
+  ): DynamicSubscription = {
+    DynamicSubscription(dynamicOwner, owner => observable.foreach(onNext)(owner))
+  }
+
+  def subscribeBus[A](
+    dynamicOwner: DynamicOwner,
+    eventStream: EventStream[A],
+    writeBus: WriteBus[A]
+  ): DynamicSubscription = {
+    DynamicSubscription(dynamicOwner, owner => writeBus.addSource(eventStream)(owner))
+  }
+}
