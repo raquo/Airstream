319d1880a89eeb1c5d14d17b73b9c1c3eaf062ad
Nikita Gazarov <nikita@raquo.com>
2020-12-25 16:23:59 -0800

    Fix: Delay operator now clears the pending queue when stopped
    Previously, if you stopped the delayed stream and then immediately started it, delayed events scheduled before the stream was stopped would fire after it was re-started if their delays did not complete while the stream was stopped.

--- src/main/scala/com/raquo/airstream/eventstream/DelayEventStream.scala
+++ src/main/scala/com/raquo/airstream/eventstream/DelayEventStream.scala
@@ -4,6 +4,7 @@ import com.raquo.airstream.core.Transaction
 import com.raquo.airstream.features.{InternalNextErrorObserver, SingleParentObservable}
 
 import scala.scalajs.js
+import scala.scalajs.js.timers.SetTimeoutHandle
 
 class DelayEventStream[A](
   override protected val parent: EventStream[A],
@@ -13,16 +14,30 @@ class DelayEventStream[A](
   /** Async stream, so reset rank */
   override protected[airstream] val topoRank: Int = 1
 
+  private val timerHandles: js.Array[SetTimeoutHandle] = js.Array()
+
   override protected[airstream] def onNext(nextValue: A, transaction: Transaction): Unit = {
-    js.timers.setTimeout(delayMillis) {
+    var timerHandle: SetTimeoutHandle = null
+    timerHandle = js.timers.setTimeout(delayMillis) {
       //println(s"> init trx from DelayEventStream.onNext($nextValue)")
+      timerHandles.splice(timerHandles.indexOf(timerHandle), deleteCount = 1) // Remove handle
       new Transaction(fireValue(nextValue, _))
     }
+    timerHandles.push(timerHandle)
   }
 
   override def onError(nextError: Throwable, transaction: Transaction): Unit = {
-    js.timers.setTimeout(delayMillis) {
+    var timerHandle: SetTimeoutHandle = null
+    timerHandle = js.timers.setTimeout(delayMillis) {
+      timerHandles.splice(timerHandles.indexOf(timerHandle), deleteCount = 1) // Remove handle
       new Transaction(fireError(nextError, _))
     }
+    timerHandles.push(timerHandle)
+  }
+
+  override protected[this] def onStop(): Unit = {
+    timerHandles.foreach(js.timers.clearTimeout)
+    timerHandles.length = 0 // Clear array
+    super.onStop()
   }
 }
