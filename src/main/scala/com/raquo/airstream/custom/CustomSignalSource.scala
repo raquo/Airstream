0af0b13e1f7dda50a40b19b61d8b8efca2a656f7
Nikita Gazarov <nikita@raquo.com>
2020-12-29 01:34:12 -0800

    New: Custom event sources, rework AjaxEventStream, and more
    - New: Signal.fromValue and Signal.fromTry
    - New: Observer.toJsFn1

---
+++ src/main/scala/com/raquo/airstream/custom/CustomSignalSource.scala
@@ -0,0 +1,41 @@
+package com.raquo.airstream.custom
+
+import com.raquo.airstream.custom.CustomSource._
+import com.raquo.airstream.signal.Signal
+
+import scala.util.{Success, Try}
+
+// @TODO[Test] needs testing
+
+/** Use this to easily create a custom signal from an external source
+  *
+  * See docs on custom sources, and [[CustomSource.Config]]
+  */
+class CustomSignalSource[A] (
+  getInitialValue: => Try[A],
+  makeConfig: (SetCurrentValue[A], GetCurrentValue[A], GetStartIndex, GetIsStarted) => CustomSource.Config,
+) extends Signal[A] with CustomSource[A] {
+
+  override protected[this] def initialValue: Try[A] = getInitialValue
+
+  override protected[this] val config: Config = makeConfig(_fireTry, tryNow, getStartIndex, getIsStarted)
+}
+
+object CustomSignalSource {
+
+  def apply[A](
+    initial: => A
+  )(
+    config: (SetCurrentValue[A], GetCurrentValue[A], GetStartIndex, GetIsStarted) => Config
+  ): Signal[A] = {
+    new CustomSignalSource[A](Success(initial), config)
+  }
+
+  def fromTry[A](
+    initial: => Try[A]
+  )(
+    config: (SetCurrentValue[A], GetCurrentValue[A], GetStartIndex, GetIsStarted) => Config
+  ): Signal[A] = {
+    new CustomSignalSource[A](initial, config)
+  }
+}
