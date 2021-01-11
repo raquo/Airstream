60ea41ef32c7af386245fdfe41ccb7bca64eeb7a
Nikita Gazarov <nikita@raquo.com>
2021-01-10 22:34:10 -0800

    API: Make throttle behave in a standard way. Fixes #66

--- src/main/scala/com/raquo/airstream/timing/DebounceEventStream.scala
+++ src/main/scala/com/raquo/airstream/timing/DebounceEventStream.scala
@@ -9,30 +9,30 @@ import scala.util.Try
 
 // @TODO[Test] Verify debounce
 
-/** [[DebounceEventStream]] emits the last event emitted by `parent`, but only after `delayFromLastEventMillis` ms
-  * has elapsed since `parent` emitted the previous event. So essentially, this stream emits the parent's last event
-  * once the parent stops emitting events for a while.
+/** This stream emits the last event emitted by `parent`, but only after `intervalMs` has elapsed
+  * since `parent` emitted the previous event.
   *
-  * [[DebounceEventStream]] is an async filter, emitting some of the input events, and with a delay, whereas
-  * [[ThrottleEventStream]] acts like a synchronous filter on input events
+  * Essentially, this stream emits the parent's last event, but only once the parent stops emitting
+  * events for `intervalMs`.
+  *
+  * See also [[ThrottleEventStream]]
   */
 class DebounceEventStream[A](
   override protected[this] val parent: EventStream[A],
-  delayFromLastEventMs: Int
+  intervalMs: Int
 ) extends EventStream[A] with SingleParentObservable[A, A] with InternalTryObserver[A] {
 
   private[this] var maybeLastTimeoutHandle: js.UndefOr[SetTimeoutHandle] = js.undefined
 
-  /** All emitted events are delayed, so we fire up a new transaction and reset topoRank */
   override protected[airstream] val topoRank: Int = 1
 
   /** Every time [[parent]] emits an event, we clear the previous timer and set a new one.
-    * This stream only emits when the parent has stopped emitting for [[delayFromLastEventMs]] ms.
+    * This stream only emits when the parent has stopped emitting for [[intervalMs]] ms.
     */
   override protected[airstream] def onTry(nextValue: Try[A], transaction: Transaction): Unit = {
     maybeLastTimeoutHandle.foreach(js.timers.clearTimeout)
     maybeLastTimeoutHandle = js.defined(
-      js.timers.setTimeout(delayFromLastEventMs.toDouble) {
+      js.timers.setTimeout(intervalMs.toDouble) {
         //println(s"> init trx from DebounceEventStream.onTry($nextValue)")
         new Transaction(fireTry(nextValue, _))
       }
--- src/main/scala/com/raquo/airstream/timing/ThrottleEventStream.scala
+++ src/main/scala/com/raquo/airstream/timing/ThrottleEventStream.scala
@@ -1,36 +1,83 @@
 package com.raquo.airstream.timing
 
-import com.raquo.airstream.core.EventStream
-import com.raquo.airstream.misc.FilterEventStream
+import com.raquo.airstream.common.{InternalTryObserver, SingleParentObservable}
+import com.raquo.airstream.core.{EventStream, Transaction}
 
 import scala.scalajs.js
+import scala.scalajs.js.timers.SetTimeoutHandle
+import scala.util.Try
 
-// @TODO[Test] Verify throttling
-
-/** [[ThrottleEventStream]] emits the next event only if more than `intervalMillis` ms
-  * has elapsed since this stream emitted the previous event.
+/** [[ThrottleEventStream]] emits at most one event per `intervalMs`.
+  * - All events are emitted in a new transaction, after an async delay, even if the delay is zero ms
+  * - Any incoming event is scheduled to be emitted as soon as possible, but no sooner than `intervalMs`
+  *   after the last event that was actually emitted by the throttled stream
+  * - When an event is scheduled to be emitted, any event that was previously scheduled is cancelled
+  *   (that's the nature of throttling, you only get at most one event within `intervalMs`)
+  * - Errors are propagated in the same manner
+  * - Stopping the stream cancels scheduled events and makes it forget everything that happened before.
   *
-  * This stream emits an error if the parent stream emits an error (Note: no throttling applied in that case)
-  *
-  * [[ThrottleEventStream]] acts like a synchronous filter on input events, whereas
-  * [[DebounceEventStream]] while also filtering input events, emits the events with a delay.
+  * See also See also [[DebounceEventStream]]
   */
-object ThrottleEventStream {
+class ThrottleEventStream[A](
+  override protected[this] val parent: EventStream[A],
+  intervalMs: Int,
+  leading: Boolean
+) extends EventStream[A] with SingleParentObservable[A, A] with InternalTryObserver[A] {
 
-  def apply[A](parent: EventStream[A], intervalMs: Int): EventStream[A] = {
-    var lastEventTimeMillis: Double = 0
+  private[this] var lastEmittedEventMs: js.UndefOr[Double] = js.undefined
 
-    new FilterEventStream[A](parent, passes = _ => {
-      val currentTimeMillis = js.Date.now()
-      val timeSinceLastEventMillis = currentTimeMillis - lastEventTimeMillis
+  /** Note: we unset this after it's done */
+  private[this] var maybeFirstTimeoutHandle: js.UndefOr[SetTimeoutHandle] = js.undefined
 
-      val shouldEmit = timeSinceLastEventMillis >= intervalMs
+  private[this] var maybeLastTimeoutHandle: js.UndefOr[SetTimeoutHandle] = js.undefined
 
-      if (shouldEmit) {
-        lastEventTimeMillis = currentTimeMillis
+  override protected[airstream] val topoRank: Int = 1
+
+  override protected[airstream] def onTry(nextValue: Try[A], transaction: Transaction): Unit = {
+
+    val nowMs = js.Date.now()
+
+    val remainingMs = lastEmittedEventMs.fold(
+      ifEmpty = if (leading) 0 else intervalMs
+    )(
+      lastEventMs => {
+        val msSinceLastEvent = nowMs - lastEventMs
+        js.Math.max(intervalMs - msSinceLastEvent.toInt, 0)
       }
+    )
 
-      shouldEmit
-    })
+    if (leading && lastEmittedEventMs.isEmpty) {
+      // #Note lastEmittedEventMs is an approximation (compare to the `else` case), I hope that doesn't bite us
+      lastEmittedEventMs = js.defined(nowMs)
+
+      maybeFirstTimeoutHandle = js.defined(
+        js.timers.setTimeout(0) {
+          maybeFirstTimeoutHandle = js.undefined
+          //println(s"> init trx from leading ThrottleEventStream.onTry($nextValue)")
+          new Transaction(fireTry(nextValue, _))
+        }
+      )
+
+    } else {
+      maybeLastTimeoutHandle.foreach(js.timers.clearTimeout)
+
+      maybeLastTimeoutHandle = js.defined(
+        js.timers.setTimeout(remainingMs.toDouble) {
+          lastEmittedEventMs = js.defined(js.Date.now()) // @TODO Should this fire now, or inside the transaction below?
+          //println(s"> init trx from ThrottleEventStream.onTry($nextValue)")
+          new Transaction(fireTry(nextValue, _))
+        }
+      )
+    }
   }
+
+  override protected[this] def onStop(): Unit = {
+    maybeFirstTimeoutHandle.foreach(js.timers.clearTimeout)
+    maybeLastTimeoutHandle.foreach(js.timers.clearTimeout)
+    maybeFirstTimeoutHandle = js.undefined
+    maybeLastTimeoutHandle = js.undefined
+    lastEmittedEventMs = js.undefined
+    super.onStop()
+  }
+
 }
