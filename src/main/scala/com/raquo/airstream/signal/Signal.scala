5c204a5c27ed1f449c5b24d8d056651dd0016049
Victor Borja <vborja@apache.org>
2021-01-27 12:36:48 -0600

    Make a Signal forget its internal memory when stopped.
    This branch changes Signal.onStop to make it forget its
    internal memory when all observers have been removed. Causing it
    to re-compute its "initialValue" again if the Signal is re-started.
    
    This way, dependent signals (eg, MapSignal) can ask their parent
    signal for the latest value upon restarting, and elements that read
    these dependent signals can now observe the correct state when re-mounted.
    
    Fixes #43

--- src/main/scala/com/raquo/airstream/signal/Signal.scala
+++ src/main/scala/com/raquo/airstream/signal/Signal.scala
@@ -137,6 +137,11 @@ trait Signal[+A] extends Observable[A] {
     super.onStart()
   }
 
+  override protected[this] def onStop(): Unit = {
+    super.onStop()
+    maybeLastSeenCurrentValue = js.undefined
+  }
+
   // @TODO[API] Use pattern match instead when isInstanceOf performance is fixed: https://github.com/scala-js/scala-js/issues/2066
   override protected def onAddedExternalObserver(observer: Observer[A]): Unit = {
     super.onAddedExternalObserver(observer)
