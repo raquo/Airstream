dc564313c9f5f41218342a83bd32176de2b9b37e
Nikita Gazarov <nikita@raquo.com>
2021-05-04 21:01:36 -0700

    Build: Scala 3.0.0-RC3 support

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,11 +6,13 @@ object Versions {
 
   val Scala_3_RC2 = "3.0.0-RC2"
 
+  val Scala_3_RC3 = "3.0.0-RC3"
+
   val ScalaJsDom = "1.1.0"
 
   val Tuplez = "0.3.6"
 
-  val ScalaTest = "3.2.7"
+  val ScalaTest = "3.2.8"
 
   val JsDom = "16.4.0"
 }
