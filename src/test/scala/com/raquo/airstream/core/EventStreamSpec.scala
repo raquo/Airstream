9955c35c6181e66dd7012c6950b09ddc27a6df23
Arman Bilge <armanbilge@gmail.com>
2023-11-16 02:01:59 +0000

    Add `EventStream.fromFlowPublisher`

--- src/test/scala/com/raquo/airstream/core/EventStreamSpec.scala
+++ src/test/scala/com/raquo/airstream/core/EventStreamSpec.scala
@@ -6,6 +6,7 @@ import com.raquo.airstream.fixtures.{Effect, TestableOwner}
 import com.raquo.airstream.ownership.Owner
 import org.scalactic.anyvals.NonEmptyList
 
+import java.util.concurrent.Flow
 import scala.collection.mutable
 
 class EventStreamSpec extends UnitSpec {
@@ -49,6 +50,37 @@ class EventStreamSpec extends UnitSpec {
     val sub2 = signal.foreach(newValue => effects += Effect("obs2", newValue))
 
     effects.toList shouldBe (3 +: range).map(i => Effect("obs2", i))
+    effects.clear()
+  }
+
+  it("EventStream.fromFlowPublisher") {
+
+    class RangePublisher(r: Range) extends Flow.Publisher[Int] {
+      def subscribe(subscriber: Flow.Subscriber[_ >: Int]): Unit = {
+        val subscription = new Flow.Subscription {
+          def request(n: Long) = r.foreach(subscriber.onNext(_))
+          def cancel() = ()
+        }
+        subscriber.onSubscribe(subscription)
+      }
+    }
+
+    implicit val owner: Owner = new TestableOwner
+
+    val range = 1 to 3
+    val stream = EventStream.fromFlowPublisher(new RangePublisher(range))
+
+    val effects = mutable.Buffer[Effect[_]]()
+    val sub1 = stream.foreach(newValue => effects += Effect("obs1", newValue))
+
+    effects.toList shouldBe range.map(i => Effect("obs1", i))
+    effects.clear()
+
+    sub1.kill()
+
+    val sub2 = stream.foreach(newValue => effects += Effect("obs2", newValue))
+
+    effects.toList shouldBe range.map(i => Effect("obs2", i))
     effects.clear()
   }
 
