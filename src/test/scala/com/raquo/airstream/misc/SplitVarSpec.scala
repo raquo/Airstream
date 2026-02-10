raquo/Airstream

commit c677448d0de1b8a07c5d5e751598d530408d8d26

API: Remove `strict` param of SplitChild
This param used to add a noop observer to child split signals. It was used for splitting vars.

I'm not sure why it was needed there in the first place. It does not seem to do anything:
 - The child signal's logic is internal, not user-provided, and it's ok for it to be lazy (I think?)
 - The child signal isn't typed as a strict signal, so we don't get .now() from it
 - The child signal is fed into LazyDerivedVar, which is intended to be lazy anyway
 - No tests fail after I removed this logic

I think this was either a half-done attempt at stricti child signals, or some misunderstanding on my part.

I don't like the sketchy code in SplitSignal that runs with strict=true, and seeing no reason for it, happy to get rid of it.

src/main/scala/com/raquo/airstream/split/SplitSignal.scala
@@ -34,7 +34,6 @@ class SplitSignal[M[_], Input, Output, Key](
   project: KeyedStrictSignal[Key, Input] => Output,
   splittable: Splittable[M],
   duplicateKeysConfig: DuplicateKeysConfig = DuplicateKeysConfig.default,
-  strict: Boolean = false // #TODO `false` default for now to keep compatibility with 17.0.0 - consider changing in 18.0.0 #nc[split]
 ) extends SingleParentSignal[M[Input], M[Output]] {
 
   private val distinctF = distinctOp(new DistinctOps.Ops[Input])
@@ -153,10 +152,6 @@ class SplitSignal[M[_], Input, Output, Key](
               }
             ).distinctTry(isSame = distinctF)
 
-          if (isStarted && strict) {
-            inputSignal.addInternalObserver(emptyObserver, shouldCallMaybeWillStart = true)
-          }
-
           val newOutput = project(inputSignal)
 
           (inputSignal, newOutput)
@@ -176,10 +171,6 @@ class SplitSignal[M[_], Input, Output, Key](
     memoized.keys.foreach { memoizedKey =>
       if (!nextKeys.contains(memoizedKey)) {
         // dom.console.log(s"${this} memoized.remove ${memoizedKey}")
-        if (strict) {
-          val inputSignal = memoized(memoizedKey)._2
-          inputSignal.removeInternalObserver(emptyObserver)
-        }
         memoized.remove(memoizedKey)
       }
     }
@@ -191,43 +182,5 @@ class SplitSignal[M[_], Input, Output, Key](
     }
 
     nextOutputs
-  }
-
-  override protected[this] def onStart(): Unit = {
-    if (strict) {
-      parent.tryNow().foreach { inputs =>
-        // splittable.foreach is perhaps less efficient that memoized.keys,
-        // but it has a predictable order that users expect.
-        splittable.foreach(inputs, (input: Input) => {
-          val memoizedKey = key(input)
-          val maybeInputSignal = memoized.get(memoizedKey).map(_._2)
-          maybeInputSignal.foreach { inputSignal =>
-            // - If inputSignal not found because `parent.tryNow()` and `memoized`
-            //   are temporarily out of sync, it should be added later just fine
-            //   when they sync up.
-            // - Typical pattern is to call Protected.maybeWillStart(inputSignal) in onWillStart,
-            //   but our SplitSignal does not actually depend on these inputSignal-s, so I think
-            //   it's ok to do both onWillStart and onStart for them here.
-            inputSignal.addInternalObserver(emptyObserver, shouldCallMaybeWillStart = true)
-          }
-        })
-      }
-    }
-    super.onStart()
-  }
-
-  override protected[this] def onStop(): Unit = {
-    if (strict) {
-      // memoized.keys has no defined order, so we don't want to
-      // use it for starting subscriptions, but for stopping them,
-      // seems fine.
-      // This way we make sure to remove observers from exactly
-      // all items that are in `memoized`, no more, no less.
-      memoized.keys.foreach { memoizedKey =>
-        val inputSignal = memoized(memoizedKey)._2
-        inputSignal.removeInternalObserver(emptyObserver)
-      }
-    }
-    super.onStop()
   }
 }

src/main/scala/com/raquo/airstream/split/SplittableVar.scala
@@ -35,8 +35,7 @@ class SplittableVar[M[_], Input](
         project(childVar)
       },
       splittable,
-      duplicateKeys,
-      strict = true
+      duplicateKeys
     )
   }
 
@@ -64,7 +63,6 @@ class SplittableVar[M[_], Input](
       },
       splittable,
       DuplicateKeysConfig.noWarnings, // No need to check for duplicates – we know the keys are good.?
-      strict = true
     )
   }
 
@@ -104,7 +102,6 @@ class SplittableVar[M[_], Input](
       },
       splittable.splittable,
       duplicateKeys,
-      strict = true
     )
   }
 }

commit c762c87e68e23da0c027bf4913ce3bee2c5fa7f3

Fix: SplitChildSignal must be strict - regression caused by c677448d0de1b8a07c5d5e751598d530408d8d26
I have now documented the rationale for the empty observer and added a test to Airstream for it.
Previously only a test in Laminar verified this.

src/main/scala/com/raquo/airstream/combine/CombineSignalN.scala
@@ -20,8 +20,9 @@ class CombineSignalN[A, Out](
   override protected val topoRank: Int = Protected.maxTopoRank(0, parents) + 1
 
   override protected[this] val parentObservers: JsArray[InternalParentObserver[_]] = {
-    parents.map { parent =>
+    parents.mapWithIndex{ (parent, ix) =>
       InternalParentObserver.fromTry[A](parent, (_, trx) => {
+        _parentLastUpdateIds.update(ix, Protected.lastUpdateId(parent)) // #TODO[Test] Note 100% sure that we need to be doing this. No test catches the difference right now.
         onInputsReady(trx)
       })
     }

src/main/scala/com/raquo/airstream/common/SingleParentSignal.scala
@@ -8,7 +8,9 @@ import com.raquo.airstream.split.SplitChildSignal
 import scala.util.Try
 
 /** A simple stream that only has one parent. */
-trait SingleParentSignal[I, O] extends WritableSignal[O] with InternalTryObserver[I] {
+trait SingleParentSignal[I, O]
+extends WritableSignal[O]
+with InternalTryObserver[I] {
 
   protected[this] val parent: Observable[I]
 
@@ -25,20 +27,33 @@ trait SingleParentSignal[I, O] extends WritableSignal[O] with InternalTryObserve
   protected[this] var _parentLastUpdateId: Int = -1
 
   /** Note: this is overridden in:
-    *  - [[SignalFromStream]] because parent can be a stream, and it has cacheInitialValue logic
     *  - [[SplitChildSignal]] because its parent is a special timing stream, not the real parent
+    *  - [[SignalFromStream]] because parent can be a stream, and it has cacheInitialValue logic
     */
   override protected def onWillStart(): Unit = {
     // dom.console.log(s"${this} > onWillStart (SPS)")
     Protected.maybeWillStart(parent)
+    if (peekWhetherParentHasUpdated().contains(true)) {
+      val newParentLastUpdateId = Protected.lastUpdateId(parent.asInstanceOf[Signal[_]])
+      updateCurrentValueFromParent(
+        currentValueFromParent(),
+        newParentLastUpdateId
+      )
+    }
+  }
+
+  /** Returns:
+    *  - Some(true) if parent is signal and has updated
+    *  - Some(false) if parent is signal and has NOT updated
+    *  - None if parent is stream (impossible to get this info from a stream)
+    */
+  protected def peekWhetherParentHasUpdated(): Option[Boolean] = {
     if (parentIsSignal) {
       val newParentLastUpdateId = Protected.lastUpdateId(parent.asInstanceOf[Signal[_]])
-      if (newParentLastUpdateId != _parentLastUpdateId) {
-        updateCurrentValueFromParent(
-          currentValueFromParent(),
-          newParentLastUpdateId
-        )
-      }
+      val parentUpdated = newParentLastUpdateId != _parentLastUpdateId
+      Some(parentUpdated)
+    } else {
+      None
     }
   }
 

src/main/scala/com/raquo/airstream/core/Named.scala
@@ -15,8 +15,10 @@ trait Named {
     */
   protected[this] var maybeDisplayName: js.UndefOr[String] = js.undefined
 
+  protected[this] def displayClassName: String = getClass.getSimpleName
+
   /** This is the method that subclasses override to preserve the user's ability to set custom display names. */
-  protected def defaultDisplayName: String = s"${getClass.getSimpleName}@${hashCode()}"
+  protected def defaultDisplayName: String = s"${displayClassName}@${hashCode()}"
 
   /** Override [[defaultDisplayName]] instead of this, if you need to. */
   final override def toString: String = displayName

src/main/scala/com/raquo/airstream/core/WritableSignal.scala
@@ -13,7 +13,7 @@ trait WritableSignal[A] extends Signal[A] with WritableObservable[A] {
 
   protected def setCurrentValue(newValue: Try[A]): Unit = {
     val isInitial = maybeLastSeenCurrentValue.isEmpty
-    // println(s"${this} > setCurrentValue > ${newValue}, isInitial = ${isInitial}")
+    // dom.console.log(s"> ${this} > setCurrentValue > ${newValue}, isInitial = ${isInitial}")
     if (!isInitial) {
       _lastUpdateId = Signal.nextUpdateId()
     }
@@ -27,7 +27,7 @@ trait WritableSignal[A] extends Signal[A] with WritableObservable[A] {
   //  mixing DistinctSignal with LazyStrictSignal in one class instance.
   /** Note: Initial value is only evaluated if/when needed (when there are observers) */
   override protected[airstream] def tryNow(): Try[A] = {
-    // dom.console.log(s"${this} > tryNow (maybeLastSeenCurrentValue = ${maybeLastSeenCurrentValue})")
+    // dom.console.log(s"> ${this} > tryNow (maybeLastSeenCurrentValue = ${maybeLastSeenCurrentValue})")
     maybeLastSeenCurrentValue.getOrElse {
       // #TODO[Integrity] I'm not sure if updating `_lastUpdateId` here is right.
       //  - I expected `0` to indicate "initial value" (no events emitted yet),
@@ -54,7 +54,7 @@ trait WritableSignal[A] extends Signal[A] with WritableObservable[A] {
   }
 
   override protected def fireTry(nextValue: Try[A], transaction: Transaction): Unit = {
-    // dom.console.log(s"$this > FIRE > $nextValue")
+    // dom.console.log(s"> $this > FIRE > $nextValue")
 
     setCurrentValue(nextValue)
 

src/main/scala/com/raquo/airstream/core/WritableStream.scala
@@ -7,7 +7,7 @@ import scala.util.Try
 trait WritableStream[A] extends EventStream[A] with WritableObservable[A] {
 
   override protected[this] def fireValue(nextValue: A, transaction: Transaction): Unit = {
-    // println(s"$this > FIRE > $nextValue")
+    // dom.console.log(s"> $this > FIRE > $nextValue")
 
     // === CAUTION ===
     // The following logic must match Signal's fireTry! It is separated here for performance.
@@ -35,7 +35,7 @@ trait WritableStream[A] extends EventStream[A] with WritableObservable[A] {
   }
 
   override protected[this] def fireError(nextError: Throwable, transaction: Transaction): Unit = {
-    // println(s"$this > FIRE > $nextError")
+    // dom.console.log(s"> $this > FIRE > $nextError")
 
     // === CAUTION ===
     // The following logic must match Signal's fireTry! It is separated here for performance.

src/main/scala/com/raquo/airstream/debug/DebuggerSignal.scala
@@ -6,11 +6,13 @@ import com.raquo.airstream.core.AirstreamError.DebugError
 
 import scala.util.Try
 
-/** See [[DebugOps]] and [[DebuggableSignal]] for user-facing debug methods */
+/** See [[DebugOps]] and [[DebugSignalOps]] for user-facing debug methods */
 class DebuggerSignal[A](
   override protected[this] val parent: Signal[A],
   override protected val debugger: Debugger[A]
-) extends SingleParentSignal[A, A] with DebuggerObservable[A] {
+)
+extends SingleParentSignal[A, A]
+with DebuggerObservable[A] {
 
   override protected val topoRank: Int = Protected.topoRank(parent) + 1
 

src/main/scala/com/raquo/airstream/split/KeyedDerivedVar.scala
@@ -5,7 +5,7 @@ import com.raquo.airstream.state.{LazyDerivedVar, StrictSignal, Var}
 import scala.util.Try
 
 class KeyedDerivedVar[K, ParentV, ThisV](
-  val parent: Var[ParentV],
+  parent: Var[ParentV],
   override val signal: StrictSignal[ThisV],
   override val key: K,
   updateParent: (Try[ParentV], Try[ThisV]) => Option[Try[ParentV]],

src/main/scala/com/raquo/airstream/split/KeyedStrictSignal.scala
@@ -3,7 +3,7 @@ package com.raquo.airstream.split
 import com.raquo.airstream.core.CoreOps
 import com.raquo.airstream.distinct.{DistinctOps, DistinctSignal}
 import com.raquo.airstream.misc.MapSignal
-import com.raquo.airstream.state.{StrictSignal, WritableStrictSignal}
+import com.raquo.airstream.state.{LazyStrictSignal, StrictSignal}
 
 import scala.util.Try
 
@@ -16,9 +16,13 @@ with DistinctOps[KeyedStrictSignal[K, A], A] { self =>
   override def map[B](project: A => B): KeyedStrictSignal[K, B] = {
     new MapSignal[A, B](parent = this, project, recover = None)
       with KeyedStrictSignal[K, B]
-      with WritableStrictSignal[B] {
+      with LazyStrictSignal[A, B] {
 
       override val key: K = self.key
+
+      override protected[this] def displayClassName: String = s"KeyedStrictSignal.map(key=${key})"
+
+      override protected val displayNameSuffix: String = ".map"
     }
   }
 
@@ -29,9 +33,13 @@ with DistinctOps[KeyedStrictSignal[K, A], A] { self =>
   override def distinctTry(isSame: (Try[A], Try[A]) => Boolean): KeyedStrictSignal[K, A] = {
     new DistinctSignal[A](parent = this, isSame, resetOnStop = false)
       with KeyedStrictSignal[K, A]
-      with WritableStrictSignal[A] {
+      with LazyStrictSignal[A, A] {
 
       override val key: K = self.key
+
+      override protected[this] def displayClassName: String = s"KeyedStrictSignal.distinct*(key=${key})"
+
+      override protected val displayNameSuffix: String = ".distinct*"
     }
   }
 }

src/main/scala/com/raquo/airstream/split/SplitChildSignal.scala
@@ -115,7 +115,7 @@ with SingleParentSignal[M[A], A] {
       //  I'm not sure if None is possible, but if it is, this is probably the right thing to do.
       //  I think None might be possible when evaluating this signal's initial value when starting it
       if (!droppedDuplicateEvent && maybeInitialTransaction == Transaction.currentTransaction()) {
-        // println(s">>>>> DROPPED EVENT ${freshMemoizedInput}, TRX IS ${maybeInitialTransaction}")
+        // dom.console.log(s">>>>> ${this} DROPPED EVENT ${freshMemoizedInput}, TRX IS ${maybeInitialTransaction}")
         maybeInitialTransaction = js.undefined
         droppedDuplicateEvent = true
       } else {

src/main/scala/com/raquo/airstream/split/SplitSignal.scala
@@ -62,6 +62,20 @@ class SplitSignal[M[_], Input, Output, Key](
 
   private[this] val sharedDelayedParent = new SyncDelayStream(parent, after = this)
 
+  // We add this empty observer to every child signal to make sure that they run even
+  // if the user has not subscribed to them. This is important because we expose .now()
+  // on those signals, either directly, or via Var.splitSeq.
+  // Q: Why do we need this, now that we use LazyStrictSignal-s explicitly?
+  // A: Because SplitChildSignal-s depend on a stream (sharedDelayedParent) rather than
+  //    on a signal, so their normal pulling mechanism does not work – they can't
+  //    pull fresh value from SplitSignal because there's this stream in between them.
+  // (Previously, I removed this mechanism some time between 18.0.0-M1 and 18.0.0-M2
+  //  because there was no test for this behaviour in Airstream, only in Laminar,
+  //  I've fixed that now.)
+  private[this] val strictnessObserver = new InternalTryObserver[Input] {
+    override protected def onTry(nextValue: Try[Input], transaction: Transaction): Unit = ()
+  }
+
   private[this] def memoizedProject(nextInputs: M[Input]): M[Output] = {
     // Any keys not in this set by the end of this function will be removed from `memoized` map
     // This ensures that previously memoized values are forgotten once the source observables stops emitting their inputs
@@ -148,6 +162,10 @@ class SplitSignal[M[_], Input, Output, Key](
               }
             ).distinctTry(isSame = distinctF)
 
+          if (isStarted) {
+            inputSignal.addInternalObserver(strictnessObserver, shouldCallMaybeWillStart = true)
+          }
+
           val newOutput = project(inputSignal)
 
           (inputSignal, newOutput)
@@ -167,6 +185,8 @@ class SplitSignal[M[_], Input, Output, Key](
     memoized.keys.foreach { memoizedKey =>
       if (!nextKeys.contains(memoizedKey)) {
         // dom.console.log(s"${this} memoized.remove ${memoizedKey}")
+        val inputSignal = memoized(memoizedKey)._2
+        inputSignal.removeInternalObserver(strictnessObserver)
         memoized.remove(memoizedKey)
       }
     }
@@ -178,5 +198,38 @@ class SplitSignal[M[_], Input, Output, Key](
     }
 
     nextOutputs
+  }
+
+  override protected[this] def onStart(): Unit = {
+    parent.tryNow().foreach { inputs =>
+      // splittable.foreach has predictable order that users expect (unlike memoized.keys.foreach)
+      splittable.foreach(inputs, (input: Input) => {
+        val memoizedKey = key(input)
+        val maybeInputSignal = memoized.get(memoizedKey).map(_._2)
+        maybeInputSignal.foreach { inputSignal =>
+          // - If inputSignal not found because `parent.tryNow()` and `memoized`
+          //   are temporarily out of sync, it should be added later just fine
+          //   when they sync up.
+          // - Typical pattern is to call Protected.maybeWillStart(inputSignal) in onWillStart,
+          //   but our SplitSignal does not actually depend on these inputSignal-s, so I think
+          //   it's ok to do both onWillStart and onStart for them here.
+          inputSignal.addInternalObserver(strictnessObserver, shouldCallMaybeWillStart = true)
+        }
+      })
+    }
+    super.onStart()
+  }
+
+  override protected[this] def onStop(): Unit = {
+    // memoized.keys has no defined order, so we don't want to
+    // use it for starting subscriptions, but for stopping them,
+    // seems fine.
+    // This way we make sure to remove observers from exactly
+    // all items that are in `memoized`, no more, no less.
+    memoized.keys.foreach { memoizedKey =>
+      val inputSignal = memoized(memoizedKey)._2
+      inputSignal.removeInternalObserver(strictnessObserver)
+    }
+    super.onStop()
   }
 }

src/main/scala/com/raquo/airstream/split/SplittableSeqVar.scala
@@ -30,7 +30,7 @@ class SplittableSeqVar[M[_], Input](
         val thisKey = signal.key
         val childVar = new KeyedDerivedVar[Key, M[Input], Input](
           parent = v,
-          signal = signal,
+          signal = signal, //.map(identity).map(identity), // #nc[strict] – can't map twice here, lazy pull not picking up – why?
           key = thisKey,
           updateParent = KeyedDerivedVar.standardErrorsF { (inputs, newInput) =>
             Some(splittable.findUpdate(inputs, key(_) == thisKey, newInput))

src/main/scala/com/raquo/airstream/state/LazyDerivedVar.scala
@@ -37,10 +37,12 @@ class LazyDerivedVar[ParentV, ThisV](
   override private[state] def getCurrentValue: Try[ThisV] = signal.tryNow()
 
   override private[state] def setCurrentValue(value: Try[ThisV], transaction: Transaction): Unit = {
+    // dom.console.log(s"> ${this} > setCurrentValue > ${value}")
     val maybeNextValue = Try(updateParent(parent.tryNow(), value)) match {
       case Success(nextValue) => nextValue
       case Failure(err) => Some(Failure(err))
     }
+    // dom.console.log(s"  - maybeNextValue = $maybeNextValue")
     maybeNextValue.foreach { nextValue =>
       // This can update the parent without causing an infinite loop because
       // the parent updates this derived var's signal, it does not call

src/main/scala/com/raquo/airstream/state/LazyStrictSignal.scala
@@ -18,8 +18,8 @@ import scala.util.Try
   * peekNow / pullNow https://github.com/raquo/Laminar/issues/130
   */
 trait LazyStrictSignal[I, O]
-extends StrictSignal[O]
-with SingleParentSignal[I, O] { self =>
+extends WritableStrictSignal[O]
+with SingleParentSignal[I, O] {
 
   protected val displayNameSuffix: String
 
@@ -30,13 +30,13 @@ with SingleParentSignal[I, O] { self =>
   override protected def defaultDisplayName: String = parentDisplayName + displayNameSuffix + s"@${hashCode()}"
 
   override def tryNow(): Try[O] = {
-    val newParentLastUpdateId = Protected.lastUpdateId(parent)
-    // #TODO This comparison only works when parentSignal is started or strict.
-    //  - e.g. it does not help us in `split`, it only helps us with lazyZoom.
-    if (newParentLastUpdateId != _parentLastUpdateId) {
-      // This branch can only run if !isStarted
+    // dom.console.log(s"> ${this} > tryNow")
+    if (peekWhetherParentHasUpdated().contains(true)) {
       val nextValue = currentValueFromParent()
-      updateCurrentValueFromParent(nextValue, newParentLastUpdateId)
+      updateCurrentValueFromParent(
+        nextValue,
+        Protected.lastUpdateId(parent)
+      )
       nextValue
     } else {
       super.tryNow()
@@ -79,6 +79,8 @@ object LazyStrictSignal {
 
       override protected val displayNameSuffix: String = _dns
 
+      override protected[this] def displayClassName: String = s"LazyStrictSignal.map"
+
       override protected def onTry(nextParentValue: Try[I], transaction: Transaction): Unit = {
         super.onTry(nextParentValue, transaction)
         fireTry(nextParentValue.map(project), transaction)
@@ -100,13 +102,14 @@ object LazyStrictSignal {
     val _pdn = parentDisplayName
     val _dns = displayNameSuffix
 
-    new DistinctSignal[A](
-      parentSignal, isSame, resetOnStop
-    ) with LazyStrictSignal[A, A] {
+    new DistinctSignal[A](parentSignal, isSame, resetOnStop)
+      with LazyStrictSignal[A, A] {
 
       override protected def parentDisplayName: String = _pdn
 
       override protected val displayNameSuffix: String = _dns
+
+      override protected[this] def displayClassName: String = s"LazyStrictSignal.distinct*"
     }
   }
 }

src/main/scala/com/raquo/airstream/timing/SyncDelayStream.scala
@@ -21,6 +21,7 @@ class SyncDelayStream[A](
   override protected val topoRank: Int = (Protected.topoRank(parent) max Protected.topoRank(after)) + 1
 
   override protected def onTry(nextValue: Try[A], transaction: Transaction): Unit = {
+    // dom.console.log(s"> ${this} > onTry > ${nextValue}")
     if (!transaction.containsPendingObservable(this)) {
       transaction.enqueuePendingObservable(this)
     }
@@ -28,6 +29,7 @@ class SyncDelayStream[A](
   }
 
   override private[airstream] def syncFire(transaction: Transaction): Unit = {
+    // dom.console.log(s"> ${this} > syncFire")
     maybePendingValue.foreach { pendingValue =>
       maybePendingValue = js.undefined
       fireTry(pendingValue, transaction)

src/test/scala/com/raquo/airstream/misc/SplitVarSpec.scala
@@ -967,4 +967,61 @@ class SplitVarSpec extends UnitSpec with BeforeAndAfter {
       // effects.clear()
     }
   }
+
+  it("unused child var behaviour") {
+
+    // If user does not actually subscribe to `fooVar`, we want to make
+    // sure that pulling its value with .now() still produces correct
+    // current value.
+
+    val owner = new TestableOwner
+
+    val effects: mutable.Buffer[String] = mutable.Buffer()
+
+    val fooVars: mutable.Map[String, Var[Foo]] = mutable.Map()
+
+    val foosVar = Var[List[Foo]](Nil).setDisplayName("foosVar")
+
+    foosVar.splitSeq(_.id) { fooVar =>
+      val id = fooVar.key
+      fooVar.setDisplayName(s"fooVar-${id}")
+      effects += s"create-${fooVar.now().toString}"
+      fooVars.update(id, fooVar)
+      Element(id, fooVar.signal)
+    }.foreach(_ => ())(owner)
+
+    assertEquals(effects.toList, Nil)
+
+    // --
+
+    foosVar.set(Foo("1", 10) :: Foo("2", 10) :: Foo("3", 10) :: Nil)
+
+    assertEquals(
+      effects.toList,
+      List(
+        """create-Foo(1,10)""",
+        """create-Foo(2,10)""",
+        """create-Foo(3,10)""",
+      )
+    )
+    effects.clear()
+
+    // --
+
+    foosVar.update(list => list.updated(0, list.head.copy(version = 20)))
+
+    assertEquals(effects.toList, Nil)
+
+    assertEquals(fooVars("1").now(), Foo("1", 20))
+
+    // --
+
+    fooVars("1").set(Foo("1", 30))
+
+    assertEquals(effects.toList, Nil)
+
+    assertEquals(fooVars("1").now(), Foo("1", 30))
+    assertEquals(fooVars("2").now(), Foo("2", 10))
+    assertEquals(fooVars("3").now(), Foo("3", 10))
+  }
 }