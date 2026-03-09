2e1e453c73556824f0bed4ceaf12cd822d61bde5
Alec Dorrington <alecdorrington@gmail.com>
2026-03-09 20:26:37 +0100

    Fixed inconsistencies and removed many -Recover method versions.

--- src/main/scala/com/raquo/airstream/scan/ScanLeftSignalOps.scala
+++ src/main/scala/com/raquo/airstream/scan/ScanLeftSignalOps.scala
@@ -42,7 +42,7 @@ trait ScanLeftSignalOps[+Self[+_], +A] extends ScanLeftOps[Self, Self, A] {
 
   /**
    * Creates a [[Signal]] that combines all updates from the parent [[Observable]] using `fn`,
-   * and emits the accumulated value every time that the parent stream emits.s
+   * and emits the accumulated value every time that the parent [[Signal]] emits.
    *
    * @param makeInitial
    *   A function for creating the initial value to use before the first update,
@@ -81,9 +81,70 @@ trait ScanLeftSignalOps[+Self[+_], +A] extends ScanLeftOps[Self, Self, A] {
   @inline final def scanLeftRecover[B](initial: Try[B])(fn: (Try[B], Try[A]) => Try[B]): Self[B] =
     scanLeftRecover[B](fn(initial, _))(fn)
 
+  /**
+   * Creates a [[Signal]] that combines all updates from the parent [[Signal]] using `fn`,
+   * and emits the accumulated value every time that the parent [[Signal]] emits.
+   *
+   * The initial value of the resulting [[Signal]] is the initial value of the parent [[Signal]].
+   *
+   * @param fn
+   *   A binary operator that takes
+   *   the previously accumulated value (on the left) and
+   *   the next update from the parent [[Observable]] (on the right),
+   *   and produces the next accumulated value.
+   *
+   * @tparam B
+   *   The type of the accumulated value and the resulting [[Signal]].
+   *
+   * @return
+   *   A [[Signal]] that emits the accumulated value every time that the parent [[Observable]] emits.
+   *
+   * @note
+   *   It is safe for `fn` to throw an exception,
+   *   in which case it will be propagated through the error channel of the resulting [[Signal]].
+   *
+   * @see
+   *   [[reduceLeftRecover]] for a version of this method with manual error recovery.
+   *
+   * @see
+   *   [[scanLeft]] for a version of this method with an explicit initial value.
+   */
   @inline final def reduceLeft[B >: A](fn: (B, A) => B): Self[B] = {
     scanLeft(identity[B])(fn)
   }
+
+  /**
+   * Creates a [[Signal]] that combines all updates from the parent [[Signal]] using `fn`,
+   * and emits the accumulated value every time that the parent [[Signal]] emits.
+   *
+   * The initial value of the resulting [[Signal]] is the initial value of the parent [[Signal]].
+   *
+   * @param fn
+   *   A binary operator that takes
+   *   the previously accumulated value (on the left) and
+   *   the next update from the parent [[Observable]] (on the right),
+   *   and produces the next accumulated value.
+   *
+   * @tparam B
+   *   The type of the accumulated value and the resulting [[Signal]].
+   *
+   * @return
+   *   A [[Signal]] that emits the accumulated value every time that the parent [[Observable]] emits.
+   *
+   * @note
+   *   It is not safe for `fn` to throw an exception.
+   *   It is assumed that the user is responsible for wrapping any exceptions in [[Try]].
+   *   If an uncaught exception nevertheless occurs, Airstream will likely crash.
+   *
+   * @see
+   *   [[reduceLeft]] for a version of this method that guards against exceptions,
+   *   the use of which is recommended instead if you don't need manual error recovery.
+   *
+   * @see
+   *   [[scanLeftRecover]] for a version of this method with an explicit initial value.
+   */
+  @inline final def reduceLeftRecover[B >: A](fn: (Try[B], Try[A]) => Try[B]): Self[B] =
+    scanLeftRecover[B](tryA => tryA.map(a => a: B))(fn)
 
   @deprecated("foldLeft was renamed to scanLeft", "15.0.0-M1")
   def foldLeft[B](makeInitial: A => B)(fn: (B, A) => B): Self[B] = scanLeft(makeInitial)(fn)
--- src/main/scala/com/raquo/airstream/scan/ScanLeftStreamOps.scala
+++ src/main/scala/com/raquo/airstream/scan/ScanLeftStreamOps.scala
@@ -29,9 +29,6 @@ trait ScanLeftStreamOps[+A] extends ScanLeftOps[Signal, EventStream, A] {
    *   in which case it will be propagated through the error channel of the resulting [[Observable]].
    *
    * @see
-   *   [[reduceLeftOptionRecover]] for a version of this method with manual error recovery.
-   *
-   * @see
    *   [[reduceLeft]] for a version of this method where the result isn't wrapped in [[Option]].
    */
   @inline final def reduceLeftOption[B >: A](fn: (B, A) => B): Signal[Option[B]] = {
@@ -40,40 +37,6 @@ trait ScanLeftStreamOps[+A] extends ScanLeftOps[Signal, EventStream, A] {
       case (Some(previous), next) => Some(fn(previous, next))
     }
   }
-
-  /**
-   * Creates a [[Signal]] that combines all events from the parent [[EventStream]] using `fn`,
-   * and emits the accumulated value every time that the parent [[EventStream]] emits.
-   *
-   * @param fn
-   *   A binary operator that takes
-   *   the previously accumulated value (on the left) and
-   *   the next event from the parent [[EventStream]] (on the right),
-   *   and produces the next accumulated value.
-   *
-   * @tparam B
-   *   The type of the accumulated value and the resulting [[Signal]].
-   *
-   * @return
-   *   A [[Signal]] that emits the accumulated value every time that the parent [[EventStream]] emits.
-   *   The contents are wrapped in [[Option]],
-   *   which is [[None]] precisely when the parent [[EventStream]] has not emitted anything yet.
-   *
-   * @note
-   *   It is not safe for `fn` to throw an exception.
-   *   It is assumed that the user is responsible for wrapping any exceptions in [[Try]].
-   *   If an uncaught exception nevertheless occurs, Airstream will likely crash.
-   *
-   * @see
-   *   [[reduceLeftOption]] for a version of this method that guards against exceptions,
-   *   the use of which is recommended instead if you don't need manual error recovery.
-   *
-   * @see
-   *   [[reduceLeftRecover]] for a version of this method where the result isn't wrapped in [[Option]].
-   */
-  // @inline final def reduceLeftOptionRecover[B >: A](fn: (Try[B], Try[A]) => Try[B]): Signal[Option[B]] = {
-  //  reduceLeftOption(unrecoverable(fn))
-  // }
 
   /**
    * Creates a [[Signal]] that combines all events from the parent [[EventStream]] using `fn`,
@@ -102,9 +65,6 @@ trait ScanLeftStreamOps[+A] extends ScanLeftOps[Signal, EventStream, A] {
    *   in which case it will be propagated through the error channel of the resulting [[Observable]].
    *
    * @see
-   *   [[reduceLeftDefaultRecover]] for a version of this method with manual error recovery.
-   *
-   * @see
    *   [[reduceLeft]] for a version of this method where no default is required.
    *
    * @see
@@ -115,13 +75,8 @@ trait ScanLeftStreamOps[+A] extends ScanLeftOps[Signal, EventStream, A] {
   }
 
   /**
-   * Creates a [[Signal]] that combines all events from the parent [[EventStream]] using `fn`,
+   * Creates an [[EventStream]] that combines all events from the parent [[EventStream]] using `fn`,
    * and emits the accumulated value every time that the parent [[EventStream]] emits.
-   *
-   * @param default
-   *   The default value to use before the first event from the parent [[EventStream]].
-   *   After the first event, the default value is ignored and the accumulated value is emitted instead.
-   *   The default value is not incorporated into the accumulated value in any way.
    *
    * @param fn
    *   A binary operator that takes
@@ -130,30 +85,22 @@ trait ScanLeftStreamOps[+A] extends ScanLeftOps[Signal, EventStream, A] {
    *   and produces the next accumulated value.
    *
    * @tparam B
-   *   The type of the accumulated value and the resulting [[Signal]].
+   *   The type of the accumulated value and the resulting [[EventStream]].
    *
    * @return
-   *   A [[Signal]] that emits the accumulated value every time that the parent [[EventStream]] emits.
-   *   The `default` value is used until the first event arrives.
+   *   An [[EventStream]] that emits the accumulated value every time that the parent [[EventStream]] emits,
+   *   after the first event.
    *
    * @note
-   *   It is not safe for `fn` to throw an exception.
-   *   It is assumed that the user is responsible for wrapping any exceptions in [[Try]].
-   *   If an uncaught exception nevertheless occurs, Airstream will likely crash.
+   *   It is safe for `fn` to throw an exception,
+   *   in which case it will be propagated through the error channel of the resulting [[EventStream]].
    *
    * @see
-   *   [[reduceLeftDefault]] for a version of this method with manual error recovery.
+   *   [[scanLeft]] for a version of this method with an explicit initial value.
    *
    * @see
-   *   [[reduceLeftRecover]] for a version of this method where no default is required.
-   *
-   * @see
-   *   [[scanLeftRecover]] for a version of this method where the default value is used to seed the accumulator.
+   *   [[reduceLeftOption]] for a version of this method where the result is wrapped in [[Option]].
    */
-  // @inline final def reduceLeftDefaultRecover[B >: A](default: Try[B])(fn: (Try[B], Try[A]) => Try[B]): Signal[B] = {
-  //  reduceLeftDefault(default.get)(unrecoverable(fn))
-  // }
-
   @inline final def reduceLeft[B >: A](fn: (B, A) => B): EventStream[B] = {
     reduceLeftOption(fn).updates.collect { case Some(value) => value }
   }
