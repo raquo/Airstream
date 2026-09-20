5be97bf855eef6b76caebec50740af6588a12e64
Nikita Gazarov <nikita@raquo.com>
2026-09-19 17:56:08 -0700

    Perf: Apply optimisations similar to #165 to other uses of hasDuplicateTupleKeys

--- src/main/scala/com/raquo/airstream/util/package.scala
+++ src/main/scala/com/raquo/airstream/util/package.scala
@@ -1,5 +1,7 @@
 package com.raquo.airstream
 
+import com.raquo.ew.JsSet
+
 import scala.util.{Failure, Try}
 
 package object util {
@@ -16,8 +18,20 @@ package object util {
 
   val always: Any => Boolean = _ => true
 
-  def hasDuplicateTupleKeys(tuples: Seq[(?, ?)]): Boolean = {
-    tuples.size != tuples.map(_._1).toSet.size
+  /** Returns true if two or more of the `items` map to the same key.
+    *
+    * Note: `@inline` should eliminate the allocation of `key` lambda.
+    *
+    * #Note: keys are compared using JS `===` semantics, i.e. by reference
+    *  for objects, NOT using Scala `equals`. Only use this for identity-keyed
+    *  checks such as Var / WriteBus de-duplication.
+    */
+  @inline def hasDuplicateKeys[A](items: Seq[A])(key: A => Any): Boolean = {
+    val seenKeys = JsSet.empty[Any]
+    items.foreach { item =>
+      seenKeys.add(key(item))
+    }
+    items.size != seenKeys.size
   }
 
   /** Like `Try(v).flatten`, but avoids allocating another `Success`. */
